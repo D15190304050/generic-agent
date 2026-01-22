f564eedc9c36ef7adda1310bcd9f52c6a8c9e09f
DinoStark <15190304050@163.com>
2026-01-22 23:55:00 +0800

    Context

--- src/main/java/stark/coderaider/genericagent/context/BudgetResult.java
+++ src/main/java/stark/coderaider/genericagent/context/BudgetResult.java
@@ -1,5 +1,23 @@
 package stark.coderaider.genericagent.context;
 
-public record BudgetResult(MessageList messageList, boolean overflowed)
+public class BudgetResult
 {
+    private final MessageList messageList;
+    private final boolean overflowed;
+
+    public BudgetResult(MessageList messageList, boolean overflowed)
+    {
+        this.messageList = messageList;
+        this.overflowed = overflowed;
+    }
+
+    public MessageList getMessageList()
+    {
+        return messageList;
+    }
+
+    public boolean isOverflowed()
+    {
+        return overflowed;
+    }
 }
